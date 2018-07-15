79d0e691b0d2f2c9b2c4142b7269c98cfb98eddd
chenzeshenga <chenzeshenga@163.com>
2018-07-15 20:49:49 +0800

    Initial commit

---
+++ com/chenzeshenga/user_input.py
@@ -0,0 +1,12 @@
+name = input("Name:")
+age = input("Age:")
+job = input("Job:")
+hometown = input("Hometown:")
+info = """
+----------info of %s-----------
+Name:       %s
+Age:        %s
+Job:        %s
+Hometown:   %s
+""" % (name, name, age, job, hometown)
+print(info)
